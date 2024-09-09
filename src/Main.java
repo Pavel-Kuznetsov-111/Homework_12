ca9082a9802f08c811db0927cee87c5eac6bc8c7
Pavel-Kuznetsov-111 <gilrok@rambler.ru>
2024-09-09 16:39:44 +0300

    Просьба проверить правильность выполнения задания Homework_12.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.io.OutputStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author bookOne = new Author("Иван ", "Тургенеев ");
+        Author bookTwo = new Author("Михаил ", "Шолохов ");
+        Book one = new Book("Отцы и дети ", bookOne, 1861);
+        Book two = new Book("Донские рассказы ", bookTwo, 1926);
+        System.out.println("Название книги - " + one.getBookName() + "Автор - " + bookOne.getFirstName() + "  " + bookOne.getLastName() + " Год публикации - " + one.getBookAge());
+        System.out.println("Название книги - " + two.getBookName() + "Автор - " + bookTwo.getFirstName() + "  " + bookTwo.getLastName() + " Год публикации - " + two.getBookAge());
+
+
     }
 }
+
+
+
+
+
