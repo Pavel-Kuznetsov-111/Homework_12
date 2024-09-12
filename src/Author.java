1cefb81a181bf4cf8ddc05147d2a75db8df97bc6
Pavel-Kuznetsov-111 <gilrok@rambler.ru>
2024-09-12 15:53:51 +0300

    Просьба проверить правильность выполнения задания Homework_13.

--- src/Author.java
+++ src/Author.java
@@ -17,13 +17,22 @@ public class Author {
         return "Имя автора " + this.firstName + "Фамилия автора " + this.lastName;
     }
 
-    public boolean equals(Author other) {
+    public boolean equals(Author other, Object o) {
         if ((this.getLastName() != other.getLastName()) && (this.getFirstName() != other.getFirstName())) {
             return false;
         } else {
             return true;
         }
+        if (this == o) {
+            return true;
+        }
+        if  (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+
     }
+
+
 
     public int hashCode() {
         return java.util.Objects.hash(lastName) + java.util.Objects.hash(firstName);
--- src/Book.java
+++ src/Book.java
@@ -28,13 +28,22 @@ public class Book {
     public String toString() {
         return "Название книги " + this.bookName + " Год написания книги" + this.bookAge;
     }
-    public boolean equals(Book other) {
+
+    public boolean equals(Book other, Object o) {
         if ((this.getBookName() != other.getBookName()) && (this.getBookName() != other.getBookName()) && (this.getBookAge() != other.getBookAge())) {
             return false;
         } else {
             return true;
         }
+        if (this == o) {
+            return true;
+        }
+        if (o == null || this.getClass() != o.getClass()) {
+            return false;
+        }
+        public int hashCode () {
+            return java.util.Objects.hash(bookName) + java.util.Objects.hash(author) + java.util.Objects.hash(bookAge);
+        }
+
 
     }
-
-}
